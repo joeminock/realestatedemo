7b50e3cfa6a5c567bfcf7f68b9ff44124d8084ca
Joe Minock <joe.minock@gmail.com>
2018-11-02 15:22:52 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get '/community', to: 'pages#community'
+  get 'community', to: 'pages#community'
 
-  get '/leasing', to: 'pages#leasing'
+  get 'leasing', to: 'pages#leasing'
 
-  get '/contact', 'pages#contact'
+  get 'contact', 'pages#contact'
 
-  get '/application', to: 'pages#application'
+  get 'application', to: 'pages#application'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
